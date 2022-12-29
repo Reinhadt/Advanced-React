154cf91c1ba5aa3e79b6cbbe1980e18c4c520480
Omar <omavaloss@gmail.com>
2022-12-29 17:15:48 -0600

    Remove unnecessary files

--- account.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Account = () => <div>account </div>;
-
-export default Account;
--- update.js
+++
@@ -1,5 +0,0 @@
-import UpdateProduct from '../components/UpdateProduct';
-
-export const UpdatePage = ({ query }) => <UpdateProduct id={query.id} />;
-
-export default UpdatePage;
